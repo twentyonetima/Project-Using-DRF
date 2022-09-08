4a6b1b6e045b3600d759724e932258e76a123062
Timofey Petrov <twentyonetima@mail.ru>
2022-09-08 03:12:16 +0300

    session-based authentication

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -14,14 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
 
 from people.views import *
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/drf-auth/', include('rest_framework.urls')),
     path('api/v1/people/', PeopleAPIList.as_view()),
     path('api/v1/people/<int:pk>/', PeopleAPIUpdate.as_view()),
     path('api/v1/peopledetail/<int:pk>/', PeopleAPIDetailView.as_view()),
